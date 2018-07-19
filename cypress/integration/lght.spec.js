80f1e969503fee603f6827420420cdc43ca746ed
Mark Noonan <markthomasnoonan@gmail.com>
2018-07-19 16:57:34 -0400

    add helper function to handle of repeated elements

--- cypress/integration/lght.spec.js
+++ cypress/integration/lght.spec.js
@@ -1,48 +1,48 @@
 describe("Loosely Goosed HTML Template", function() {
-	beforeEach(() => {
-		cy.visit("http://localhost:1234/");
-	});
+  beforeEach(() => {
+    cy.visit("http://localhost:1234/");
+  });
 
-	it("the test page loads", () => {
-		cy.contains("template");
-	});
+  it("the test page loads", () => {
+    cy.contains("template");
+  });
 
-	it("Renders the example articles", () => {
-		cy.contains("Introduction");
-		cy.contains("Advantages");
-		cy.contains("Drawbacks");
-		cy.contains("Conclusion");
-	});
+  it("Renders the example articles", () => {
+    cy.contains("Introduction");
+    cy.contains("Advantages");
+    cy.contains("Drawbacks");
+    cy.contains("Conclusion");
+  });
 
-	it("Renders a new aricle with all acceptable property types", () => {
-		cy.window().then(win => {
-			win.lght.addContentToTarget({
-				templateSelector: "#article-template",
-				targetSelector: "#article",
-				templateData: {
-					title: "Hello There",
-					anotherThing: ["array", "test"].join(""),
-					aNumber: 4000,
-					ipsum: true
-				}
-			});
-		});
-		cy.contains("Hello There");
-		cy.contains("4000");
-		cy.contains("true");
-	});
+  it("Renders a new aricle with all acceptable property types", () => {
+    cy.window().then(win => {
+      win.lght.addContentToTarget({
+        templateSelector: "#article-template",
+        targetSelector: "#article",
+        templateData: {
+          title: "Hello There",
+          anotherThing: ["array", "test"].join(""),
+          aNumber: 4000,
+          ipsum: true
+        }
+      });
+    });
+    cy.contains("Hello There");
+    cy.contains("4000");
+    cy.contains("true");
+  });
 
-	it("throws an error when trying to iterpolate an object or array", () => {
-		cy.window().then(win => {
-			expect(() => {
-				win.lght.addContentToTarget({
-					templateSelector: "#article-template",
-					targetSelector: "#article",
-					templateData: {
-						title: ["Hello There"]
-					}
-				});
-			}).to.throw();
-		});
-	});
+  it("throws an error when trying to interpolate an object or array", () => {
+    cy.window().then(win => {
+      expect(() => {
+        win.lght.addContentToTarget({
+          templateSelector: "#article-template",
+          targetSelector: "#article",
+          templateData: {
+            title: ["Hello There"]
+          }
+        });
+      }).to.throw();
+    });
+  });
 });
